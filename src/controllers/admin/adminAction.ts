cadbcdeaafc2e2b4610796036629fda9082d2a53
adityaraj-09 <itsrishabhforyou@gmail.com>
2024-08-08 13:22:41 +0530

    feat: Add updateRole API endpoint for modifying role description

--- src/controllers/admin/adminAction.ts
+++ src/controllers/admin/adminAction.ts
@@ -204,6 +204,25 @@ export const addNewRole=async (req:Request,res:Response)=>{
     }
 }
 
+export const updateRole=async (req:Request,res:Response)=>{
+    try {
+        const {description,role_code}=req.body
+        const role=await RoleModel.findOne
+        ({
+            role_code:role_code,
+        })
+        if(!role){
+            return res.status(400).json(createError(400,"error","role with this role code does not exits"))
+        }
+        role!.description=description
+        await role!.save()
+        res.status(200).json(createResponse("success",role))
+    }
+    catch (error) {
+            res.status(500).json(createError(500,"error","internal server error"))
+    }
+}
+
 export const allocateRoleMenu=async (req:Request,res:Response)=>{
     try {
         const {menu,sections,roleId}=req.body
--- src/routers/admin/index.ts
+++ src/routers/admin/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { addAdmin, addNewConfig, addNewRole, allocateRoleMenu, allocateRoleToAdmin, deleteAdmin, updatePass } from "../../controllers/admin/adminAction";
+import { addAdmin, addNewConfig, addNewRole, allocateRoleMenu, allocateRoleToAdmin, deleteAdmin, updatePass, updateRole } from "../../controllers/admin/adminAction";
 import { authenticateUser } from "../../middlewares/auth";
 import { addToMaster, deleteFieldInMaster, getFieldDataFromMaster, updateMaster, uploadInstrt } from "../../controllers/forms/masterController";
 import { getControls, getAllAdmins, getDashboard } from "../../controllers/admin/data";
@@ -19,6 +19,7 @@ adminActionRouter.delete("/:id/:userId/:roleId/delete-admin",authenticateUser(tr
 adminActionRouter.post("/:userId/update-password",authenticateUser(true),updatePass)
 
 adminActionRouter.post("/user-defs/add-role",authenticateUser(false),addNewRole)
+adminActionRouter.post("/user-defs/update-role",authenticateUser(false),updateRole)
 adminActionRouter.post("/user-defs/allocate-roleMenu",authenticateUser(false),allocateRoleMenu)
 adminActionRouter.post("/user-defs/:userId/allocate-role",authenticateUser(false),allocateRoleToAdmin)
 
@@ -33,30 +34,30 @@ adminActionRouter.get("/:schoolId/dashboard",authenticateUser(false),getDashboar
 
 // for master
 adminActionRouter.post("/master/:id/add",authenticateUser(false),uploadInstrt,addToMaster)
-adminActionRouter.post("/master/:id/:field",authenticateUser(false),getFieldDataFromMaster)
+adminActionRouter.get("/master/:id/:field",authenticateUser(false),getFieldDataFromMaster)
 adminActionRouter.post("/master/:masterId/:field/:subId/update",authenticateUser(false),uploadInstrt,updateMaster)
 adminActionRouter.delete("/master/:masterId/:field/:subId/delete",authenticateUser(false),deleteFieldInMaster)
 
 
 adminActionRouter.post("/qexam/:id/add",authenticateUser(false),addToQExamConfig)
-adminActionRouter.post("/qexam/:id/:field",authenticateUser(false),getFieldDataFromQualifyingExam)
+adminActionRouter.get("/qexam/:id/:field",authenticateUser(false),getFieldDataFromQualifyingExam)
 adminActionRouter.post("/qexam/:qexamId/:field/:subId/update",authenticateUser(false),updateQExamConfig)
 adminActionRouter.delete("/qexam/:qexamId/:field/:subId/delete",authenticateUser(false),deleteQExamConfig)
 adminActionRouter.post("/add",addNewConfig)
 
 
 adminActionRouter.post("/config/:id/add",authenticateUser(false),addToConfig)
-adminActionRouter.post("/config/:id/:field",authenticateUser(false),getFieldDataFromConfig)
+adminActionRouter.get("/config/:id/:field",authenticateUser(false),getFieldDataFromConfig)
 adminActionRouter.post("/config/:id/addField",authenticateUser(false),addFieldsToConfig)
 adminActionRouter.post("/config/:configId/:field/:subId/update",authenticateUser(false),updateConfig)
 adminActionRouter.delete("/config/:configId/:field/:subId/delete",authenticateUser(false),deleteFieldInConfig)
 
 adminActionRouter.post("/subconfig/:id/add",authenticateUser(false),addToSubject)
-adminActionRouter.post("/subconfig/:id/:field",authenticateUser(false),getFieldDataFromSubConfig)
+adminActionRouter.get("/subconfig/:id/:field",authenticateUser(false),getFieldDataFromSubConfig)
 adminActionRouter.post("/subconfig/:subconfigId/:field/:subId/update",authenticateUser(false),updateSubConfig)
 
 adminActionRouter.post("/entconfig/:id/add",authenticateUser(false),addToEnCOnfig)
-adminActionRouter.post("/entconfig/:id/:field",authenticateUser(false),getFieldDataFromEntranceConfig)
+adminActionRouter.get("/entconfig/:id/:field",authenticateUser(false),getFieldDataFromEntranceConfig)
 adminActionRouter.post("/entconfig/:entconfigId/:field/:subId/update",authenticateUser(false),updateEntConfig)
 // adminActionRouter.delete("/subconfig/:subconfigId/:field/:subId/delete",authenticateUser(false),deleteQExamConfig)
 
